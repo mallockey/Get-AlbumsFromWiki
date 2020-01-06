c5ff7bf6021e31f6661347ec7b7861394bd0a996
Josh Melo <joshuamelo1126@gmail.com>
2020-01-06 00:20:34 -0500

    Update Send-LatestAlbums.ps1

--- Send-LatestAlbums.ps1
+++ Send-LatestAlbums.ps1
@@ -1,32 +1,42 @@
-<#
-This script uses the Get-AlbumsFromWiki and Twilio to send the newest albums of the month.
-#>
+#This script uses the scraper to get the latest albums in between the lowerbound and upperbound
 function Prepare-SendString{
 	param(
 		$InputObjects
 	)
 	$String = ""
-	
+		$Counter = 1
 		$InputObjects | Foreach-Object {
 			$CurrentAlbum = $_ | Select-Object -ExpandProperty Album
 			$CurrentArtist = $_ | Select-Object -ExpandProperty Artist
 			$CurrentReleaseDate = $_ | Select-Object -ExpandProperty ReleaseDate
-			$String += "$currentAlbum by: $currentArtist released: $currentReleaseDate | `n"
+			$String += "$Counter | '$currentAlbum' by $currentArtist | $currentReleaseDate |`n"
+			$Counter++
 	}
 	return $String
 }
 
-$fromNumber = "Twilio Number"
-$toNumber = "Your Number"
+$LowerBound = (Get-Date).Day - 3
+$UpperBound = (Get-Date).Day + 6
+$fromNumber = ""
+$toNumber = "" 
 $accountSID = ''
 $authToken = ''
 $CurrentMonth =  (Get-Culture).DateTimeFormat.GetMonthName((Get-Date).Month)
-$LatestAlbums = .\Get-AlbumsFromWiki -Year (Get-Date).Year | Where-Object {$_.ReleaseDate -like "*$CurrentMonth*"}
+$LatestAlbums = C:\Scripts\Send-LatestAlbums\Get-AlbumsFromWiki.ps1 -Year (Get-Date).Year | Where-Object {$_.ReleaseDate -like "*$CurrentMonth*"}
 
-$String = Prepare-SendString $LatestAlbums
+$SendAlbums = @()
+foreach($Album in $LatestAlbums){
+	[Int]$Date = $Album.ReleaseDate -replace "[^0-9]"
+	if($Date -gt $LowerBound -and $Date -lt $UpperBound){
+		$SendAlbums += $Album
+	}
+	
+}
+
+$String = Prepare-SendString $SendAlbums
 
 if($String.Length -gt 400){
 	$String = Prepare-SendString ($LatestAlbums | Select-Object -First 3)
 }
 
-C:\Scripts\BirthdayReminder\Send-TwilioSMS.ps1 -AccountSID $accountSID -AuthToken $authToken -FromNumber $fromNumber -ToNumber $toNumber -Message $String
+C:\Scripts\Send-TwilioSMS.ps1 -AccountSID $accountSID -AuthToken $authToken -FromNumber $fromNumber -ToNumber $toNumber -Message $String
